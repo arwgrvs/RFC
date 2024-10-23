4c4282f45eb909c870bcbbc9a7fd4ed38e62bf29
arwgrvs <goal080813hgtrgna2@cbtis217.edu.mx>
2024-10-23 07:50:00 -0600

    version2

---
+++ jijijixd/jijijixd/jijijixd/MainForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-PC20
+ * Date: 22/10/2024
+ * Time: 07:32 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace jijijixd
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.txt_nom = new System.Windows.Forms.TextBox();
+			this.txt_ap1 = new System.Windows.Forms.TextBox();
+			this.txt_tel = new System.Windows.Forms.TextBox();
+			this.txt_ap2 = new System.Windows.Forms.TextBox();
+			this.txt_dir = new System.Windows.Forms.TextBox();
+			this.txt_fcn = new System.Windows.Forms.TextBox();
+			this.cmb_1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.lbl_final = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(76, 92);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(150, 35);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Nombre";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(76, 146);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(150, 35);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Apellido1";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(76, 200);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(150, 35);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Apellido2";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(76, 249);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(150, 35);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Telefono";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(76, 303);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(150, 35);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Direccion";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(76, 365);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(164, 35);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Fecha de nacimiento";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(76, 420);
+			this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(150, 35);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "Semestre";
+			// 
+			// txt_nom
+			// 
+			this.txt_nom.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.txt_nom.Location = new System.Drawing.Point(252, 89);
+			this.txt_nom.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt_nom.Name = "txt_nom";
+			this.txt_nom.Size = new System.Drawing.Size(180, 26);
+			this.txt_nom.TabIndex = 7;
+			// 
+			// txt_ap1
+			// 
+			this.txt_ap1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.txt_ap1.Location = new System.Drawing.Point(249, 142);
+			this.txt_ap1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt_ap1.Name = "txt_ap1";
+			this.txt_ap1.Size = new System.Drawing.Size(180, 26);
+			this.txt_ap1.TabIndex = 8;
+			// 
+			// txt_tel
+			// 
+			this.txt_tel.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.txt_tel.Location = new System.Drawing.Point(249, 245);
+			this.txt_tel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt_tel.Name = "txt_tel";
+			this.txt_tel.Size = new System.Drawing.Size(180, 26);
+			this.txt_tel.TabIndex = 9;
+			// 
+			// txt_ap2
+			// 
+			this.txt_ap2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.txt_ap2.Location = new System.Drawing.Point(249, 195);
+			this.txt_ap2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt_ap2.Name = "txt_ap2";
+			this.txt_ap2.Size = new System.Drawing.Size(180, 26);
+			this.txt_ap2.TabIndex = 10;
+			// 
+			// txt_dir
+			// 
+			this.txt_dir.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.txt_dir.Location = new System.Drawing.Point(249, 298);
+			this.txt_dir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt_dir.Name = "txt_dir";
+			this.txt_dir.Size = new System.Drawing.Size(180, 26);
+			this.txt_dir.TabIndex = 11;
+			// 
+			// txt_fcn
+			// 
+			this.txt_fcn.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.txt_fcn.Location = new System.Drawing.Point(249, 360);
+			this.txt_fcn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt_fcn.Name = "txt_fcn";
+			this.txt_fcn.Size = new System.Drawing.Size(180, 26);
+			this.txt_fcn.TabIndex = 13;
+			// 
+			// cmb_1
+			// 
+			this.cmb_1.FormattingEnabled = true;
+			this.cmb_1.Items.AddRange(new object[] {
+									"1",
+									"2",
+									"3",
+									"4",
+									"5",
+									"6"});
+			this.cmb_1.Location = new System.Drawing.Point(252, 417);
+			this.cmb_1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.cmb_1.Name = "cmb_1";
+			this.cmb_1.Size = new System.Drawing.Size(180, 28);
+			this.cmb_1.TabIndex = 14;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(252, 498);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(112, 35);
+			this.button1.TabIndex = 15;
+			this.button1.Text = "Calcular";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(170, 14);
+			this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(279, 35);
+			this.label8.TabIndex = 16;
+			this.label8.Text = "Calculo de RFC";
+			this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// lbl_final
+			// 
+			this.lbl_final.Location = new System.Drawing.Point(502, 257);
+			this.lbl_final.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lbl_final.Name = "lbl_final";
+			this.lbl_final.Size = new System.Drawing.Size(150, 35);
+			this.lbl_final.TabIndex = 17;
+			this.lbl_final.Text = "____________";
+			// 
+			// label10
+			// 
+			this.label10.Location = new System.Drawing.Point(453, 249);
+			this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(24, 35);
+			this.label10.TabIndex = 18;
+			this.label10.Text = "=";
+			this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(654, 552);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.lbl_final);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.cmb_1);
+			this.Controls.Add(this.txt_fcn);
+			this.Controls.Add(this.txt_dir);
+			this.Controls.Add(this.txt_ap2);
+			this.Controls.Add(this.txt_tel);
+			this.Controls.Add(this.txt_ap1);
+			this.Controls.Add(this.txt_nom);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "MainForm";
+			this.Text = "jijijixd";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label lbl_final;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox cmb_1;
+		private System.Windows.Forms.TextBox txt_fcn;
+		private System.Windows.Forms.TextBox txt_dir;
+		private System.Windows.Forms.TextBox txt_ap2;
+		private System.Windows.Forms.TextBox txt_tel;
+		private System.Windows.Forms.TextBox txt_ap1;
+		private System.Windows.Forms.TextBox txt_nom;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		
+	}
+}
---
+++ jijijixd/jijijixd/jijijixd/MainForm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-PC20
+ * Date: 22/10/2024
+ * Time: 07:32 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace jijijixd
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		string ss="";
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(txt_ap1.Text==ss||txt_ap2.Text==ss||txt_dir.Text==ss||txt_fcn.Text==ss||txt_nom.Text==ss||txt_tel.Text==ss||cmb_1.Text==ss)
+			{
+				MessageBox.Show("Datos incompletos >:v");
+			}
+			else
+			{
+				string ape1=txt_ap1.Text;
+				ape1=ape1.Substring(0, 2);
+				string fecha=txt_fcn.Text;
+				char ape2=txt_ap2.Text[0];
+				char nom=txt_nom.Text[0];
+				string dia=fecha.Substring(0,2);
+				string mes=fecha.Substring(3,2);
+				string anio=fecha.Substring(6,2);
+				string mfmf=ape1+ape2+nom+anio+mes+dia;
+				lbl_final.Text=mfmf.ToString();
+			}
+		}
+		
+	}
+}
